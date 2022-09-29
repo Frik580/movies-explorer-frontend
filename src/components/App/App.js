a308f8a5b668cc9557590d43f88a7d961611e262
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2022-09-29 19:55:59 +0300

    Сделать файловую структуру проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from "../Main/Main";
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className="root">
+    <Header />
+    <Main />
+    </div>
+  );
+}
+
+export default App;
